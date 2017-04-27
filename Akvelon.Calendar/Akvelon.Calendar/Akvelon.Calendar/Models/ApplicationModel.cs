2fccd9d81cefd7d2d87e7a01b4628a415ee122e9
philippranzhin <fillpackartnet@gmail.com>
2017-04-27 12:41:51 +0300

    Initial commit

---
+++ Akvelon.Calendar/Akvelon.Calendar/Akvelon.Calendar/Models/ApplicationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akvelon.Calendar.Models
+{
+    public class ApplicationModel
+    {
+        #region properties
+        public string Name { get; set; }
+        #endregion
+    }
+}
---
+++ Akvelon.Calendar/Akvelon.Calendar/Akvelon.Calendar/ViewModels/ApplicationVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akvelon.Calendar.Models;
+
+namespace Akvelon.Calendar.ViewModels
+{
+    public class ApplicationVM:ViewModelBase
+    {
+        #region fields
+
+        private ApplicationModel _model;        
+
+        #endregion
+
+        #region constructor
+
+        public ApplicationVM(ApplicationModel model)
+        {
+            _model = model;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ApplicationModel Model
+        {
+            get { return _model; }
+            private set
+            {
+                _model = value;
+                OnPropertyChanged("Model");
+            }
+        }
+
+        public string Name
+        {
+            get { return Model.Name; }
+            private set
+            {
+                Model.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+    }
+}
